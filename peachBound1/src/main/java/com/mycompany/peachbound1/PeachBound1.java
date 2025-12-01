48aa4981a09a01edbdc4ef819995cb0af516eb71
CHANDAN NIR <cnir1@ocdsb.ca>
2025-12-01 14:03:07 -0500

    Creation of peachBound

---
+++ peachBound1/src/main/java/com/mycompany/peachbound1/PeachBound1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.peachbound1;
+
+/**
+ *
+ * @author cnir1
+ */
+public class PeachBound1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
