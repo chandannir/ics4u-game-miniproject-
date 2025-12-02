f22e5ccae489443c6367df946a891dca4259b2b1
mesastraw <abdoalrzag@outlook.com>
2025-12-02 13:22:55 -0500

    Setup player and Inventory class

--- peachBound1/src/main/java/com/mycompany/peachbound1/Player.java
+++ peachBound1/src/main/java/com/mycompany/peachbound1/Player.java
@@ -1,13 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.peachbound1;
 
-/**
- *
- * @author clemo1
- */
+import java.util.ArrayList;
+
 public class Player {
+    private int defense_lvl;
+    private int offense_lvl;
+    private int health;
+
+    private enum weakness; // Figure out which enums will be used
+    private enum strength; 
+    private ArrayList<Enum> debuffs; 
+
+    public Player() {
+
+    }
+
+    public void openInventory() {
+
+    }
+
+    public void attack(Weapon weapon) {
+
+    }
     
+    public void useSkill(Ability ability) {
+
+    }
+
+    public void useConsumable(Consumable consumable) {
+
+    }
+
+    public void block() {
+
+    }
+
+    public void retreat() {
+
+    }
 }
---
+++ peachBound1/src/main/java/com/mycompany/peachbound1/Status.java
@@ -0,0 +1,5 @@
+
+package com.mycompany.peachbound1;
+
+
+public enum
