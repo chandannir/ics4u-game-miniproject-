3a4f02c433418e5a14c79a3192e74bafbaabad05
CHANDAN NIR <cnir1@ocdsb.ca>
2025-12-02 13:35:20 -0500

    Getter & Setter for Monster

--- peachBound1/src/main/java/com/mycompany/peachbound1/Monster.java
+++ peachBound1/src/main/java/com/mycompany/peachbound1/Monster.java
@@ -1,5 +1,6 @@
 package com.mycompany.peachbound1;
-
+import java.util.ArrayList;
+import java.util.Enumeration;
 /**
  *
  * @author cnir1
@@ -14,15 +15,87 @@ abstract class Monster {
     private double dmg;
     private int stamina;
     private int defence;
+    private int offense;
+    private DMG_TYPES weakness;
+    private DMG_TYPES strength;
+    public ArrayList<DMG_TYPES> debuffs; 
     
     // Monster Constructor
-    Monster(String name, double health, DMG_TYPES dmg_type, double dmg, int stamina, int defence){
+    Monster(String name, double health, DMG_TYPES dmg_type, double dmg, int stamina, int defence, DMG_TYPES weakness, DMG_TYPES strength, int offense){
         this.name = name;
         this.health = health;
         this.dmg_type = dmg_type;
         this.dmg = dmg;
         this.stamina = stamina;
         this.defence = defence;
+        this.weakness = weakness;
+        this.strength = strength;
+        this.offense = offense;
+        ArrayList<DMG_TYPES> debuffs = new ArrayList<DMG_TYPES>(EnumSet.allOf(DMG_TYPES.class));
+    }
+    
+    public String getName(){
+        return name;
+    }
+    public double getHealth(){
+        return health;
+    }
+    public DMG_TYPES getDmgType(){
+        return dmg_type;
+    } 
+    public double getDmg(){
+        return dmg;
+    }
+    public int getStamina(){
+        return stamina;
+    }
+    public int getDefence(){
+        return defence;
+    }
+    public int getOffense(){
+        return offense;
+    }
+    public DMG_TYPES getStrength(){
+        return strength;
+    } public DMG_TYPES getWeakness(){
+        return weakness;
+    } 
+    
+    public String setName(String name){
+        this.name = name;
+        return name;
+    }
+    public double setHealth(double health){
+        this.health = health;
+        return health; 
+    }
+    public DMG_TYPES setDmgType(DMG_TYPES dmg_type){
+        this.dmg_type = dmg_type;
+        return dmg_type;
+    } 
+    public double setDmg(double dmg){
+        this.dmg = dmg;
+        return dmg;
+    }
+    public int setStamina(int stamina){
+        this.stamina = stamina;
+        return stamina;
+    }
+    public int setDefence(int defence){
+        this.defence = defence;
+        return defence;
+    }
+    public int setOffense(int offense){
+        this.offense = offense;
+        return offense;
+    }
+    public DMG_TYPES setStrength(DMG_TYPES strength){
+        this.strength = strength;
+        return strength;
+    } 
+    public DMG_TYPES setWeakness(DMG_TYPES weakness){
+        this.weakness = weakness; 
+        return weakness;
     }
     
     abstract double attack(double dmg, DMG_TYPES dmg_type);
--- peachBound1/src/main/java/com/mycompany/peachbound1/RegularMonster.java
+++ peachBound1/src/main/java/com/mycompany/peachbound1/RegularMonster.java
@@ -8,6 +8,28 @@ package com.mycompany.peachbound1;
  *
  * @author cnir1
  */
-public class RegularMonster {
+
+// Regular Monster Class
+public class RegularMonster extends Monster{
     
+    RegularMonster(String name, double health, DMG_TYPES dmg_type, double dmg, int stamina, int defence){
+        super(name, health, dmg_type, dmg, stamina, defence);
+    }
+    
+    @Override 
+    public double attack(double dmg, DMG_TYPES dmg_type, Player p){
+        double curPlayerHealth = p.gethealth - dmg;
+        p.setHealth = curPlayerHealth;
+        return curPlayerHealth;
+       
+    }
+    @Override 
+    public double specialAttack(double dmg, DMG_TYPES dmg_type, int stamina){
+        
+    }
+        
+    @Override 
+    public void block(int defenece){
+        
+    }
 }
