5d018c6bed3e5c44cd19210c94b593f0cc864ca6
CHANDAN NIR <cnir1@ocdsb.ca>
2025-12-02 12:45:32 -0500

    monster & dmg_type enum

--- peachBound1/src/main/java/com/mycompany/peachbound1/DMG_TYPES.java
+++ peachBound1/src/main/java/com/mycompany/peachbound1/DMG_TYPES.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
  */
 package com.mycompany.peachbound1;
 
@@ -8,6 +8,6 @@ package com.mycompany.peachbound1;
  *
  * @author cnir1
  */
-public class Monster {
-
+public enum DMG_TYPES {
+    
 }
